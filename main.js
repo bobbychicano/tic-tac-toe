e16d2328523e5d1483c6311d57e6c8cc343cbb07
Robert <rescobedo12@gmail.com>
2022-04-03 15:48:58 -0700

    Create a module to store the game board in and begin setting up player and game flow objects.

---
+++ main.js
@@ -0,0 +1,23 @@
+const gameBoard = (() => {
+  const gameSpaces = document.querySelectorAll('.grid-cell');
+
+  const gameSquares = Array.from(gameSpaces);
+
+  return {gameSquares};
+
+})();
+
+// gameBoard.~somefunction~
+
+//Player Objects
+
+const playerFactory = () => {
+
+}
+
+
+//Game flow Object
+
+gameFlow = (() => {
+
+})();
