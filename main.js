1c9e69a58b58673cc7dd135175547b7adcc17e5d
Robert <rescobedo12@gmail.com>
2022-04-07 13:35:18 -0700

    Fix logic that checks for winner. My .every() function needed an explicit return added to the code.

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-
-
-
-
-
 // Gameboard Object
 const gameBoard = (() => {
   const gameCells = document.querySelectorAll(".grid-cell");
-  // Returns a nodelist so we turn that into an array
   const gameSquares = Array.from(gameCells);
 
-  return { gameCells, gameSquares };
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  return { gameSquares, winningCombinations };
 })();
 
 
@@ -26,56 +31,56 @@ const playerFactory = (name, move) => {
 
 const playerX = playerFactory('Billy', 'X');
 const playerO = playerFactory('Bob', 'O');
-  // playerO === { getName, getMove}
-  // playerO.getName() === 'Bob'
-  // playerO.getMove() === 'O'
 
 
 
 
 
-// The Gameflow object or 'displayController' as it is called in the project page
+// The Gameflow object
+// Or, the 'displayController' as it is called in the project page
 const gameFlow = (() => {
-  const winner = '';
+
+  let winner = '';
   let _currentPlayer = playerX;
 
   // The function to place(draw) the 'X's and 'O's on the tic tac toe grid
   const drawMove = function(e) {
 
-    const gridNumber = e.target.dataset.box;
-    console.log(gridNumber);
+    const cell = e.target;
+    // Old code: const gridNumber = e.target.dataset.box;
     const spanItem = document.createElement("span");
     spanItem.innerText = _currentPlayer.getMove();
-    gameBoard.gameCells[gridNumber].append(spanItem);
+    cell.append(spanItem);
+    // Old code: gameBoard.gameSquares[gridNumber].append(spanItem);
 
+    checkForWinner(_currentPlayer);
+
+    changePlayer();
+
+  };
+
+  const changePlayer = function() {
     if (_currentPlayer == playerX){
       _currentPlayer = playerO;
     } else {
       _currentPlayer = playerX;
     }
-  };
+  }
 
   // The function to check for a winner. Will be returned to be called as a method.
-  const checkForWinner = function() {
+  //My logic for checking the winner is wrong
+  const checkForWinner = function(player) {
 
-    const winningCombinations = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
+    console.log(player.getMove());
 
-    winningCombinations.forEach((combo) => {
+    gameBoard.winningCombinations.some(combo => {
+      let checkCombo = combo.every(element => {
+        return gameBoard.gameSquares[element].textContent == player.getMove();
+      })
 
-      let check = combo.every((element) => gameBoard.gameCells[element].firstElementChild.innerText === `${_currentPlayer.getMove()}`)
-
-      if (check === 'true') {
-        winner = `${_currentPlayer}`;
-        console.log(winner);
+      if (checkCombo) {
+        winner = _currentPlayer.getName();
+        console.log(winner)
       }
 
     })
@@ -85,36 +90,12 @@ const gameFlow = (() => {
 })();
 
 
-
-/*
-*
-*
-  const displayWinner() {
-    // Function that toggles a hidden element in the DOM with a certain class to suddenly be visible.
-  }
-
-gameBoard.gameSquares.forEach((arrElement) => {
-  arrElement.addEventListener("click", gameFlow.checkForWinner)
-});
-
-// Add another click event to the gameBoard.gameSquares?
-// gameFlow.checkForWinner();
-
-//try this out in a code pen maybe to see if it works
-for (let i = 0; i < gameBoard.gameSquares.length; i++) {
-
-  for (let j = 0; j < winningCombinations.length; j++) {
-
-  }
-}
-*
-*
-*/
-
-
-
-
-
 gameBoard.gameSquares.forEach((arrElement) => {
   arrElement.addEventListener("click", gameFlow.drawMove, { once: true })
 });
+
+/*
+  const displayWinner() {
+    // Function that toggles a hidden element in the DOM with a certain class to suddenly be visible.
+  }
+*/
